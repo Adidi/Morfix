f0360dfa6cf4520061632d800e707bf429f931e0
Adidi <adieloz@gmail.com>
2017-09-15 22:09:28 +0300

    better test for check is html

--- test/xhr.spec.js
+++ test/xhr.spec.js
@@ -3,8 +3,8 @@ import { searchMorfix } from '../src/js/utils/xhr';
 import cheerio from 'cheerio';
 
 describe('Morfix http call', () => {
-    let data;
-    describe('check hebrew translation with "baby"', () => {
+    let data, $;
+    describe('check hebrew translation for "baby"', () => {
         it('http status is 200', async () => {
             const result = await searchMorfix('baby');
             const { status } = result;
@@ -14,18 +14,18 @@ describe('Morfix http call', () => {
     
         it('response is html', () => {
             data.should.be.a('string');
-            data.should.contains('<html');
+            $ = cheerio.load(data);
+            $('title').length.should.be.equal(1);
         });
 
         it('hebrew translation contains "תינוק"', () => {
-            const $ = cheerio.load(data);
             const heWord0 =  $('div.heWord0');
             heWord0.length.should.be.equal(1);
             heWord0.find('div.translation_he').text().should.contains('תִּינוֹק');
         });
     });
 
-    describe('check english translation with "אמא"', () => {
+    describe('check english translation for "אמא"', () => {
         it('http status is 200', async () => {
             const result = await searchMorfix(encodeURIComponent('אמא'));
             const { status } = result;
@@ -35,11 +35,11 @@ describe('Morfix http call', () => {
     
         it('response is html', () => {
             data.should.be.a('string');
-            data.should.contains('<html');
+            $ = cheerio.load(data);
+            $('title').length.should.be.equal(1);
         });
 
         it('english translation contains "mother"', () => {
-            const $ = cheerio.load(data);
             const enWord0 = $('#translate_result0');
             enWord0.length.should.be.equal(1);
             enWord0.find('div.default_trans').text().toLowerCase().should.contains('mother');
